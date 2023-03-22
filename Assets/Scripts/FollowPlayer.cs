f2a9468ea27ee7f5f65e944ab5a3758454a46157
joyrexjrl <ricklozano@gmail.com>
2023-03-21 19:21:26 -0700

    refactored code
    removed unused code in camera section

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -14,6 +14,5 @@ public class FollowPlayer : MonoBehaviour
         targetPosition.y = Mathf.Clamp(_player.position.y, _minY, _maxY) + _offset.y;
         transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, targetPosition.y, Time.deltaTime * _yLerpSpeed), transform.position.z);
         transform.position = new Vector3(_offset.x + _player.position.x, transform.position.y, _offset.z + _player.position.z);
-        //transform.position = _player.position + _offset;
     }
 }
