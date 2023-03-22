7e9ec7a01862c0e1d7946f1a230078b7d7af644e
joyrexjrl <ricklozano@gmail.com>
2023-03-21 18:08:59 -0700

    added color changer to ground
    added color changing script to ground and anti gravity ground, this way player has something more to look at then just plain colors the whole time. colors and change times will be adjusted in time

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    MeshRenderer cubeMeshRenderer;
+    [SerializeField] [Range(0f, 1f)] float _lerpTime;
+    [SerializeField] Color[] _colors;
+
+    int _colorIndex = 0;
+    int _len;
+    float _timer = 0f;
+
+    void Awake()
+    {
+        cubeMeshRenderer = GetComponent<MeshRenderer>();
+        _len = _colors.Length;
+    }
+
+    void Update()
+    {
+        cubeMeshRenderer.material.color = Color.Lerp(cubeMeshRenderer.material.color, _colors[_colorIndex], _lerpTime * Time.deltaTime);
+
+        _timer = Mathf.Lerp(_timer, 1f, _lerpTime * Time.deltaTime);
+        if(_timer > .9f)
+        {
+            _timer = 0f;
+            _colorIndex++;
+            _colorIndex = (_colorIndex >= _len) ? 0 : _colorIndex;
+        }
+    }
+}
