b4931f0dafc36e89603cb9f9d70e5322cc54c562
joyrexjrl <ricklozano@gmail.com>
2023-03-22 01:55:04 -0700

    added pooling system, bugged
    added a pooling system to handle the amount of blocks in the game, the system it self is working to reuse and create blocks, but the movement code of the blocks is very bugged. all movement code for the blocks has been disabled for the time being,

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 
-public class BlockMovement : MonoBehaviour
+public class BlockMovement : MonoBehaviour, IPooledObject
 {
     [SerializeField] Rigidbody _blockRb;
-    [SerializeField] float _blockSpeed = 80;
+    [SerializeField] float _blockSpeed = 30;
     [SerializeField] float _gravityScale = 2f;
     [SerializeField] bool _isReverseBlock = false;
 
-    void FixedUpdate()
+    public void OnObjectSpawn()
     {
-        _blockRb.AddForce(0, 0, -_blockSpeed * Time.deltaTime, ForceMode.VelocityChange);
-        if (_isReverseBlock) _blockRb.AddForce(Vector3.up * _gravityScale * _blockRb.mass, ForceMode.Force);
-        if (transform.position.y < -2f) Destroy(gameObject);
-        if (transform.position.y > 25f && _isReverseBlock) Destroy(gameObject);
+        
+    }
+
+    void LateUpdate()
+    {
+        //_blockRb.AddForce(0, 0, -_blockSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        //if (_isReverseBlock) _blockRb.AddForce(Vector3.up * _gravityScale * _blockRb.mass, ForceMode.Force);
+        //if (transform.position.y < -2f) Destroy(gameObject);
+        //if (transform.position.y > 25f && _isReverseBlock) Destroy(gameObject);
     }
 }
--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -3,44 +3,44 @@ using UnityEngine;
 public class BlockSpawner : MonoBehaviour
 {
     [SerializeField] Transform[] _spawnPoints;
-    [SerializeField] GameObject _blockPrefab;
     [SerializeField] GameObject _jumpBlock;
-    [SerializeField] GameObject _reverseBlockPrefab;
     [SerializeField] GameObject _reverseJumpBlock;
     [SerializeField] float _timeToSpawn = 2f;
     [SerializeField] float _timeBetweenWaves = 1f;
     [SerializeField] bool _isReverseBlockSpawner = false;
 
+    ObjectPooler objectPooler;
+
+    void Awake()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
     void FixedUpdate()
     {
         if(Time.time >= _timeToSpawn)
         {
-            if (!_isReverseBlockSpawner) SpawnBlocks(_blockPrefab, _jumpBlock);
-            else SpawnBlocks(_reverseBlockPrefab, _reverseJumpBlock);
-            _timeToSpawn = Time.time + _timeBetweenWaves;
-        }
-        
-    }
+            int randomNumber = Random.Range(1, 10);
+            int randomIndex = Random.Range(0, _spawnPoints.Length);
 
-    void SpawnBlocks(GameObject blocks, GameObject jumpBlocks)
-    {
-        int randomNumber = Random.Range(1, 10);
-        int randomIndex = Random.Range(0, _spawnPoints.Length);
-        if (!_isReverseBlockSpawner)
-        {
-            for (int i = 0; i < _spawnPoints.Length; i++)
+            if (!_isReverseBlockSpawner)
             {
-                if (randomIndex != i) Instantiate(blocks, _spawnPoints[i].position, Quaternion.identity);
-                if (randomIndex == i && randomNumber <= 3) Instantiate(jumpBlocks, _spawnPoints[i].position, Quaternion.Euler(jumpBlocks.transform.rotation.x - 30, jumpBlocks.transform.position.y - 0.8f, 0));
+                for (int i = 0; i < _spawnPoints.Length; i++)
+                {
+                    if (randomIndex != i) objectPooler.SpawnFromPool("Obstacle", _spawnPoints[i].position, Quaternion.identity); //Instantiate(blocks, _spawnPoints[i].position, Quaternion.identity);
+                    if (randomIndex == i && randomNumber <= 3) objectPooler.SpawnFromPool("JumpObstacle", _spawnPoints[i].position, Quaternion.Euler(_jumpBlock.transform.rotation.x - 30, _jumpBlock.transform.position.y - 0.8f, 0)); //Instantiate(jumpBlocks, _spawnPoints[i].position, Quaternion.Euler(jumpBlocks.transform.rotation.x - 30, jumpBlocks.transform.position.y - 0.8f, 0));
+                }
             }
-        }
-        else
-        {
-            for (int i = 0; i < _spawnPoints.Length; i++)
+            else
             {
-                if (randomIndex != i && _isReverseBlockSpawner) Instantiate(blocks, _spawnPoints[i].position, Quaternion.identity);
-                if (randomIndex == i && randomNumber <= 3 && _isReverseBlockSpawner) Instantiate(jumpBlocks, _spawnPoints[i].position, Quaternion.Euler(jumpBlocks.transform.rotation.x + 30, jumpBlocks.transform.position.y + 0.8f, 0));
+                for (int i = 0; i < _spawnPoints.Length; i++)
+                {
+                    if (randomIndex != i && _isReverseBlockSpawner) objectPooler.SpawnFromPool("ReverseObstacle", _spawnPoints[i].position, Quaternion.identity); //Instantiate(blocks, _spawnPoints[i].position, Quaternion.identity);
+                    if (randomIndex == i && randomNumber <= 3 && _isReverseBlockSpawner) objectPooler.SpawnFromPool("ReverseJumpObstacle", _spawnPoints[i].position, Quaternion.Euler(_reverseJumpBlock.transform.rotation.x + 30, _reverseJumpBlock.transform.position.y + 0.8f, 0)); //Instantiate(jumpBlocks, _spawnPoints[i].position, Quaternion.Euler(jumpBlocks.transform.rotation.x + 30, jumpBlocks.transform.position.y + 0.8f, 0));
+                }
             }
-        }
+
+            _timeToSpawn = Time.time + _timeBetweenWaves;
+        }        
     }
 }
