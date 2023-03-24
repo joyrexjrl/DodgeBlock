171e04035c087eae0d6fd31bc58108cac0d73092
joyrexjrl <ricklozano@gmail.com>
2023-03-24 14:02:41 -0700

    changed player movement code
    adjusted reverse jump block speed again to try and match other block speeds, changed player movement code to use a, d, arrow keys, and controller movements.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _fallDeathHeight = -1f;
     [SerializeField] float _reverseFallDeathHeight = 25.2f;
 
+    void Update()
+    {
+        Input.GetAxisRaw("Horizontal");
+    }
+
     void FixedUpdate()
-    {        
-        if (Input.GetKey("d"))
-        {
-            _rb.AddForce(_sidewaysMovement * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("a"))
-        {
-            _rb.AddForce(-_sidewaysMovement * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        if(_rb.position.y < _fallDeathHeight || _rb.position.y > _reverseFallDeathHeight)
+    {
+        MovePlayer();
+        if (_rb.position.y < _fallDeathHeight || _rb.position.y > _reverseFallDeathHeight)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
     }
+
+    void MovePlayer()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            _rb.AddForce(_sidewaysMovement * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            _rb.AddForce(-_sidewaysMovement * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+
+    /*if (Input.GetKey("d"))
+        {
+            _rb.AddForce(_sidewaysMovement* Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    if (Input.GetKey("a"))
+        {
+            _rb.AddForce(-_sidewaysMovement * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    if (_rb.position.y < _fallDeathHeight || _rb.position.y > _reverseFallDeathHeight)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }*/
 }
