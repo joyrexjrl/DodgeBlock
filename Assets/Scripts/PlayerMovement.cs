bc48ce1c544042053dae879c95b2fab94abeeecf
joyrexjrl <ricklozano@gmail.com>
2023-04-01 00:53:23 -0700

    slowly implementing unitys player control system
    changed old movement to new player control system, got gamepad and keyboard re set up but with new system, still working on trying to get the menus working with new system.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,17 +8,29 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _fallDeathHeight = -1f;
     [SerializeField] float _reverseFallDeathHeight = 25.2f;
 
+    PlayerInput playerInput;
+    PlayerControl playerContol;
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerContol = new PlayerControl();
+        playerContol.Gameplay.Enable();
+    }
+
     void Update() => Input.GetAxisRaw("Horizontal");
 
     void FixedUpdate()
     {
-        MovePlayer();
+        Vector2 inputVector = playerContol.Gameplay.PlayerMove.ReadValue<Vector2>();
+        _rb.AddForce(_sidewaysMovement * Time.fixedDeltaTime * new Vector3(inputVector.x, 0, inputVector.y), ForceMode.VelocityChange);
+        //MovePlayer();
         if (_rb.position.y < _fallDeathHeight || _rb.position.y > _reverseFallDeathHeight) FindObjectOfType<GameManager>().EndGame();
     }
 
-    void MovePlayer()
+    /*void MovePlayer()
     {
         if(Input.GetAxisRaw("Horizontal") > 0) _rb.AddForce(_sidewaysMovement * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
         else if(Input.GetAxisRaw("Horizontal") < 0) _rb.AddForce(-_sidewaysMovement * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);        
-    }
+    }*/
 }
