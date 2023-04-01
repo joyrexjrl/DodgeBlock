5fbff02130b57142c585664589a9ae686ba95ff6
joyrexjrl <ricklozano@gmail.com>
2023-04-01 16:40:14 -0700

    implemented store amounts
    added more functionality to the store scene, now properly shows the amount of points that has been gained through gameplay, have not tested this part yet but the points should correctly be subtracted and re saved back to the player prefs when something is bought

--- Assets/Scripts/GameStore.cs
+++ Assets/Scripts/GameStore.cs
@@ -1,7 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameStore : MonoBehaviour
 {
-    public void PlayGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    [SerializeField] TextMeshProUGUI _spendablePoints;
+
+    float _currencyPoints;
+    public float CurrencyPoints
+    {
+        get { return _currencyPoints; }
+        set
+        {
+            _currencyPoints = value;
+            PlayerPrefs.SetFloat("currentScoreForCurrency", value);
+            PlayerPrefs.Save();
+            UpdateSpendablePointsText();
+        }
+    }
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("currentScoreForCurrency"))
+        {
+            _currencyPoints = PlayerPrefs.GetFloat("currentScoreForCurrency");
+            UpdateSpendablePointsText();
+        }
+        else _currencyPoints = 0f;
+    }
+
+    void UpdateSpendablePointsText() => _spendablePoints.text = $"Spendable points: {_currencyPoints:0.00}";
+
+    public void PlayGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    public void PurchaseItem(float cost)
+    {
+        if (_currencyPoints >= cost)
+        {
+            _currencyPoints -= cost;
+            CurrencyPoints = _currencyPoints; // this line updates the PlayerPrefs value
+        }
+    }
 }
